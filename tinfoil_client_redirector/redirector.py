39279e63af805e66980827e4deaf102b1909637c
eXhumer <exhumer@exhumer.cc>
2021-03-05 23:53:07 -0700

    Return better output for non-tinfoil clients

--- tinfoil_client_redirector/redirector.py
+++ tinfoil_client_redirector/redirector.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint
+from flask import Blueprint, request
 from tinfoil_client_redirector.db import get_db
 from tinfoil_client_redirector.utils import valid_tinfoil_request
 
@@ -10,4 +10,4 @@ redirector_bp = Blueprint("redirector", __name__)
 def redirector():
     if valid_tinfoil_request(get_db()):
         return json.dumps({"success": "Hello Tinfoil :)"})
-    return "Hello World"
+    return f"<h1>Add {request.url_root} to Tinfoil to use!</h1>"
