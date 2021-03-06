29b05cd555f67bcaa257c91353a2efe87365419c
eXhumer <exhumer@exhumer.cc>
2021-03-06 01:44:22 -0700

    Update function arguments

--- tinfoil_client_redirector/redirector.py
+++ tinfoil_client_redirector/redirector.py
@@ -1,6 +1,5 @@
 import json
 from flask import Blueprint, request
-from tinfoil_client_redirector.db import get_db
 from tinfoil_client_redirector.utils import valid_tinfoil_request
 
 redirector_bp = Blueprint("redirector", __name__)
@@ -8,6 +7,6 @@ redirector_bp = Blueprint("redirector", __name__)
 
 @redirector_bp.route("/")
 def redirector():
-    if valid_tinfoil_request(get_db()):
+    if valid_tinfoil_request():
         return json.dumps({"success": "Hello Tinfoil :)"})
     return f"<h1>Add {request.url_root} to Tinfoil to use!</h1>"
