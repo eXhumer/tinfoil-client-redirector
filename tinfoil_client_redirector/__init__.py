0a513b7eb9182a6daef76d4f2fc5183b54b5b4a7
eXhumer <exhumer@exhumer.cc>
2021-03-06 03:44:24 -0700

    Load app config from JSON instance file silently * Remove secret key

--- tinfoil_client_redirector/__init__.py
+++ tinfoil_client_redirector/__init__.py
@@ -2,17 +2,13 @@ from flask import Flask
 from pathlib import Path
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
         MONGO_URI="mongodb://localhost:27017/tinfoil_client_db",
     )
 
-    if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    app.config.from_json("config.json", silent=True)
 
     instance_path = Path(app.instance_path)
     if not instance_path.is_dir():
